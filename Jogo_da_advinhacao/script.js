39929e3e4bcfeee84b82753dede29e86131d1575
Fernanda Dias <fernandadias9@hotmail.com>
2023-10-02 16:28:32 -0300

    Update script.js
    code refactoring

--- Jogo_da_advinhacao/script.js
+++ Jogo_da_advinhacao/script.js
@@ -1,32 +1,52 @@
+// variáveis
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
-const randomNumber = Math.round(Math.random() * 10);
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+let randomNumber = Math.round(Math.random() * 10);
 let attempts = 1;
 
+// eventos
+btnTry.addEventListener("click", toTry);
+btnReset.addEventListener("click", resetClick);
+document.addEventListener('keydown', enterToReset);
+
+// funções
 function toTry(event) {
   event.preventDefault(); // não faça o padrão, o padrão aqui seria atualizar a página
 
   const inputNumber = document.querySelector("#inputNumber");
-
-  if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add("hide");
-    screen2.classList.remove("hide");
-
-    document
-      .querySelector(".screen2 h2")
-      .innerText = `Acertou em ${attempts} tentativas.`
+  
+  if (Number(inputNumber.value) < 0 || Number(inputNumber.value) > 10 || inputNumber.value == ""){
+    alert("Por favor insira um número entre 0 e 10.");
   }
 
-  inputNumber.value = "";
-  attempts++;
+  if (Number(inputNumber.value) == randomNumber) {
+        toggleScreen();
+    
+        screen2.querySelector(" h2")
+          .innerText = `Acertou em ${attempts} tentativas.`
+      }
+    
+      if (inputNumber.value != "" && Number(inputNumber.value) >= 0 && Number(inputNumber.value) <= 10){
+        attempts++;     
+      }     
+  inputNumber.value = ""; 
 }
 
-const btnTry = document.querySelector("#btnTry");
-const btnReset = document.querySelector("#btnReset");
-
-btnTry.addEventListener("click", toTry);
-btnReset.addEventListener("click", function () {
-  screen1.classList.remove("hide");
-  screen2.classList.add("hide");
+function resetClick() {
+  toggleScreen();
   attempts = 1;
-});
+  randomNumber = Math.round(Math.random() * 10);
+}
+
+function toggleScreen () {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
+
+function enterToReset(e) {
+  if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+    resetClick();
+  }
+}
